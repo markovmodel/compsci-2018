0766b83ccdc9b1b80d99ede893c4235cd4a14ed2
Christoph Wehmeyer <christoph.wehmeyer@fu-berlin.de>
2018-11-29 09:59:22 +0100

    more tests and example for 2d laplacian

--- projects/project_4/poisson.py
+++ projects/project_4/poisson.py
@@ -24,3 +24,35 @@ def create_laplacian_1d(nx, lx, pbc=True):
         laplacian[0, -1] = 0
         laplacian[-1, 0] = 0
     return laplacian
+
+def create_laplacian_2d(nx, ny, lx, ly, pbc=True):
+    """Generates 2d Laplacian matrix with respect to grid spacing.   
+       
+    Creates 4d array to show components of laplace operator at each grid point
+    and fills it as per discetized laplace operator taking care of
+    periodic boundary conditions.  
+    In the end reshapes it into 2d laplaction matrix .  
+    
+    Parameters:
+        nx(integer): number of grid points in X-direction
+        ny(integer): number of grid points in Y-direction
+        hx(float): grid-spacing in X-direction
+        hy(float): grid-spacing in Y-direction
+    
+    Returns:
+        Laplacian matrix of size(nx * ny, nx * ny)  
+    """
+    if not pbc:
+        raise NotImplementedError(
+            'Nonperiodic laplacians are not yet available')
+    laplacian = np.zeros(shape=(nx, ny, nx, ny), dtype=np.float64)
+    mx = (nx / lx)**2
+    my = (ny / ly)**2
+    for x in range(nx):
+        for y in range(ny):
+            laplacian[x, y, x, y] = -2.0 * (mx + my)
+            laplacian[x, y, (x + 1) % nx, y] += mx
+            laplacian[x, y, (x - 1) % nx, y] += mx
+            laplacian[x, y, x, (y + 1) % ny] += my
+            laplacian[x, y, x, (y - 1) % ny] += my
+    return laplacian.reshape(nx * ny, nx * ny)
--- projects/project_4/test_poisson_blind.py
+++ projects/project_4/test_poisson_blind.py
@@ -14,11 +14,13 @@ try:
     from .poisson_3 import create_laplacian_2d as create_laplacian_2d_3
 except ImportError:
     print('Cannot import poisson_3.create_laplacian_2d')
+from .poisson import create_laplacian_2d as create_laplacian_2d_r
 
 
 @pytest.mark.parametrize(
     'module,function', [
         ('.poisson','create_laplacian_1d'),
+        ('.poisson','create_laplacian_2d'),
         ('.poisson_1','create_laplacian_2d'),
         ('.poisson_2','create_laplacian_2d'),
         ('.poisson_3','create_laplacian_2d')])
@@ -37,12 +39,16 @@ def test_module_and_interface(module, function):
         [
             'create_laplacian_2d_1',
             'create_laplacian_2d_2',
-            'create_laplacian_2d_3'],
+            'create_laplacian_2d_3',
+            'create_laplacian_2d_r'],
         [5, 10, 20],
         [5, 10, 20],
         [1.0, 3.0],
         [1.0, 3.0])])
 def test_consistency(create_laplacian_2d, nx, ny, lx, ly):
+    """Test consistency between solving Poisson's equation and
+    recomputing the charge density.
+    """
     laplacian = eval(
         f'{create_laplacian_2d}({nx}, {ny}, {lx}, {ly}, pbc=True)')
     assert laplacian.ndim == 2, \
@@ -58,3 +64,35 @@ def test_consistency(create_laplacian_2d, nx, ny, lx, ly):
         -np.dot(laplacian, phi).reshape(nx, ny),
         rho,
         rtol=1e-5, atol=1e-5)
+
+
+@pytest.mark.parametrize(
+    'create_laplacian_2d,nx,ny', [
+    (func, nx, ny)
+    for func, nx, ny in product(
+        [
+            'create_laplacian_2d_1',
+            'create_laplacian_2d_2',
+            'create_laplacian_2d_3',
+            'create_laplacian_2d_r'],
+        [50, 100],
+        [50, 100])])
+def test_cossin(create_laplacian_2d, nx, ny):
+    """Test a known solution of Poisson's esquation.
+    """
+    laplacian = eval(
+        f'{create_laplacian_2d}({nx}, {ny}, {2 * np.pi}, {2 * np.pi}, pbc=True)')
+    assert laplacian.ndim == 2, \
+        f'laplacian has wrong dimension: {laplacian.ndim}'
+    assert laplacian.shape[0] == nx * ny, \
+        f'laplacian has wrong first shape: {laplacian.shape[0]}'
+    assert laplacian.shape[1] == nx * ny, \
+        f'laplacian has wrong second shape: {laplacian.shape[1]}'
+    gx = np.linspace(-np.pi, np.pi, nx, endpoint=False)
+    gy = np.linspace(-np.pi, np.pi, ny, endpoint=False)
+    x, y = np.meshgrid(gx, gy, indexing='ij')
+    phi = np.cos(x) + np.sin(y)
+    np.testing.assert_allclose(
+        -np.dot(laplacian, phi.reshape(-1)).reshape(nx, ny),
+        phi,
+        rtol=1e-2, atol=5e-2)
